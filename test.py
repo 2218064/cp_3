2cc54a6e042e149646fdf5a5355f90e59c691886
2218064 <namh2801@naver.com>
2023-09-16 01:24:15 +0900

    3주차 수업내용
    온라인 수업내용

--- test.py
+++ test.py
@@ -1,1 +1,66 @@
-print("hello world")
+name = "나민하"
+print(name)
+
+a = b = "Alice"
+print(a, b)
+
+c, d = 2, "홍길동"
+print(c, d)
+
+a = '그는 말했다. "밥 먹자"'
+print(a)
+
+a = "그는 말했다."
+b = '"밥 먹자."'
+print(a+b)
+
+print("-"*50)
+print("사과"*50)
+
+a = "사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과"
+Len(a)
+
+string_a = "가나다라마바사"
+print(string_a[0:4])
+
+type(string_a)
+
+number = 1
+type(string_a)
+
+a = 4
+b = 2
+
+print(a + b, a - b, a * b, a / b)
+
+print(a**b, a % b, a // b)
+
+a = ["배열 1번", 2, "배열 3"]
+print(a[0:2])
+
+c = [1, 2, 3]
+
+c.append(4)
+
+print(c)
+
+c.remove(4)
+print(c)
+
+d = [2, 3, 4, 5, 3]
+d.remove(3)
+
+print(d)
+
+person = {"name" : "alice", "age" : 25}
+
+person["name"] = "홍길동"
+
+a = True
+b = False
+type(a)
+
+if a == True :
+    print("hello world")
+
+    
